161db96166bf99a61b072307a46101e0adf9bcab
Daniel Platt <github@ofdan.co.uk>
2015-06-05 23:17:46 +0100

    user data fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $languages = ['en', 'fr', 'ru', 'es', 'de'];
+
+        foreach ($languages as $language) {
+            $this->setupUser($manager, 'user-'.$language, 'user');
+        }
+
+        $this->setupAdminUser($manager, 'admin', 'admin');
+    }
+
+    public function setupAdminUser(ObjectManager $manager, $username, $password)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        // Create our user and set details
+        $userAdmin = $userManager->createUser();
+        $userAdmin->setUsername($username);
+        $userAdmin->setEmail($username.'@domain.test');
+        $userAdmin->setPlainPassword($password);
+        //$userAdmin->setPassword('3NCRYPT3D-V3R51ON');
+        $userAdmin->setEnabled(true);
+        $userAdmin->setRoles(array('ROLE_ADMIN'));
+
+        // Update the user
+        $userManager->updateUser($userAdmin, true);
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+
+        $this->addReference($username, $userAdmin);
+    }
+
+    public function setupUser(ObjectManager $manager, $username, $password)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        // Create our user and set details
+        $user = $userManager->createUser();
+        $user->setUsername($username);
+        $user->setEmail($username.'@domain.test');
+        $user->setPlainPassword($password);
+        //$user->setPassword('3NCRYPT3D-V3R51ON');
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_USER'));
+
+        // Update the user
+        $userManager->updateUser($user, true);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $this->addReference($username, $user);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1; // the order in which fixtures will be loaded
+    }
+}
