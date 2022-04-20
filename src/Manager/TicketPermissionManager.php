56788d5ae2f192e2ac640ec2f11e652f5bf071b8
Daniel Platt <github@ofdan.co.uk>
2022-04-20 17:11:03 +0100

    Fully rename Permission service to manager

--- src/Manager/TicketPermissionManager.php
+++ src/Manager/TicketPermissionManager.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Service;
+namespace App\Manager;
 
 use Hackzilla\Bundle\TicketBundle\Manager\PermissionManagerInterface;
 use Hackzilla\Bundle\TicketBundle\Manager\PermissionManagerTrait;
 
-class TicketPermissionService implements PermissionManagerInterface
+class TicketPermissionManager implements PermissionManagerInterface
 {
     use PermissionManagerTrait;
 }
